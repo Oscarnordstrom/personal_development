069e871267ce4227b4e869fae39286d4cf2676d8
oscarnordstrom <oscarjnordstrom>
2023-04-30 14:22:33 +0200

    Removed exercises from book im not reading anymore

--- cpp/books/a_complete_guide_to_programming_in_cpp/exercises/1_fundamentals/1_exercise/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Oh what" << std::endl;
-    std::cout << "a happy day!" << std::endl;
-    std::cout << "Oh yes," << std::endl;
-    std::cout << "what a happy day!" << std::endl;
-    return 0;
-}
--- cpp/books/a_complete_guide_to_programming_in_cpp/exercises/2_fundamental_types/2_exercise/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "\tI" << std::endl;
-    std::cout << "\t\t\"RUSH\"" << std::endl;
-    std::cout << "\t\t\t\\TO\\" << std::endl;
-    std::cout << "\t\tAND" << std::endl;
-    std::cout << "\t/FRO/" << std::endl;
-}
