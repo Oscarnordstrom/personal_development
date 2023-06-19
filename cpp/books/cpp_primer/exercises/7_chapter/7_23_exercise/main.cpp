1ff82569d707addb5ee4ef897f033dd8cd0254fa
oscarnordstrom <oscarjnordstrom@gmail.com>
2023-06-19 16:31:21 +0200

    7_23 done

---
+++ cpp/books/cpp_primer/exercises/7_chapter/7_23_exercise/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+
+class Screen {
+public:
+  typedef string::size_type position;
+
+  Screen() = default;
+  Screen(position ht, position wd, char c = ' ')
+      : height(ht), width(wd), contents(ht * wd, c){};
+  Screen(position ht, position wd, char c)
+      : height(ht), width(wd), contents(ht * wd, c){};
+
+private:
+  position cursor = 0;
+  position height = 0, width = 0;
+  string contents;
+};
+
+int main() { return 0; }
