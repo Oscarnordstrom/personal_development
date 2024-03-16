c3611995ddb6684f53263cf0725f778c4c8dd0b8
oscarnordstrom <oscarjnordstrom@gmail.com>
2024-03-16 12:55:57 +0100

    Adds recursive insertion sort implementation

--- algorithms-datastructures/sorting/modules/insertion_sort.cpp
+++ algorithms-datastructures/sorting/modules/insertion_sort.cpp
@@ -1,4 +1,5 @@
 #include "./print.h"
+#include <cstdint>
 #include <vector>
 
 template <typename T> void _insertion_sort_decreasing(std::vector<T> &v) {
@@ -39,14 +40,40 @@ template <typename T> void _insertion_sort_increasing(std::vector<T> &v) {
   }
 }
 
+template <typename T>
+void _insert_increasing(std::vector<T> &v,
+                        typename std::vector<uint64_t>::size_type q) {
+  T key = v[q];
+  auto j = q - 1;
+  while (j >= 0 && v[j] > key) {
+    v[j + 1] = v[j];
+    j -= 1;
+  }
+  v[j + 1] = key;
+};
+
+template <typename T>
+void _insertion_sort_increasing_recursive(
+    std::vector<T> &v, typename std::vector<T>::size_type q) {
+  if (q == 0) {
+    return;
+  }
+  _insertion_sort_increasing_recursive(v, q - 1);
+  _insert_increasing(v, q);
+}
+
 template <typename T> void insertion_sort(std::vector<T> &v, bool decreasing) {
   if (decreasing) {
     _insertion_sort_decreasing(v);
   } else {
-    _insertion_sort_increasing(v);
+    _insertion_sort_increasing_recursive(v, v.size() - 1);
   }
 }
 
 template void insertion_sort<uint64_t>(std::vector<uint64_t> &v, bool);
+template void _insertion_sort_increasing_recursive<uint64_t>(
+    std::vector<uint64_t> &v, std::vector<uint64_t>::size_type);
+template void _insert_increasing<uint64_t>(std::vector<uint64_t> &v,
+                                           std::vector<uint64_t>::size_type);
 template void _insertion_sort_decreasing<uint64_t>(std::vector<uint64_t> &v);
 template void _insertion_sort_increasing<uint64_t>(std::vector<uint64_t> &v);
